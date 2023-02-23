f6e1884166962cf5d224d7fb00ee832e83776e2d
Daniil Telegin <daniil.collis@gmail.com>
2023-02-23 23:09:15 +0300

    Renamed base class and added extra fields

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,15 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 
-class ToDo(models.Model):
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField('Задание', max_length=300)
+    description = models.TextField('Описание', null=True, blank=True)
     is_complete = models.BooleanField('Выполнено', default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Задание'
         verbose_name_plural = 'Задания'
+        ordering = ['is_complete']
 
     def __str__(self):
         return self.title
