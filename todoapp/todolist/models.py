5aaa33789d9d8c2990e828c7c7e64753db697bcc
Daniil Telegin <daniil.collis@gmail.com>
2023-02-23 18:35:06 +0300

    Edited title name.

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -4,12 +4,12 @@ from django.db import models
 # Create your models here.
 
 class ToDo(models.Model):
-    title = models.CharField('Задача', max_length=300)
+    title = models.CharField('Задание', max_length=300)
     is_complete = models.BooleanField('Выполнено', default=False)
 
     class Meta:
-        verbose_name = 'Задача'
-        verbose_name_plural = 'Задачи'
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
 
     def __str__(self):
         return self.title
