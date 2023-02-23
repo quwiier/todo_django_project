ffa48562ed32880c654364f7ae898493180b3370
Daniil Telegin <daniil.collis@gmail.com>
2023-02-23 17:56:01 +0300

    Added and registered model with tasks.

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
+class ToDo(models.Model):
+    title = models.CharField('Задача', max_length=300)
+    is_complete = models.BooleanField('Выполнено', default=False)
 
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
